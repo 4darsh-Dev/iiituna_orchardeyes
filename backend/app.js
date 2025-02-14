d1b4350bb8822815cb5545d78410740fad898ca1
AmanSingh494 <amanforwork1@gmail.com>
2025-02-14 16:58:14 +0530

    Added ui components

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 import express from 'express'
 import cors from 'cors'
+import twilio from 'twilio'
 
 dotenv.config()
 
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,20 +1,120 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, BrowserRouter } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 import './App.css'
-import HomePage from './pages/HomePage'
+import { Routes, useLocation, Route, useNavigate } from 'react-router-dom'
+import { useAuth0 } from '@auth0/auth0-react'
+import Navbar from './components/Navbar'
+import LandingPage from './pages/LandingPage'
+import ProfilePage from './components/OwnerPage'
+
+import OrchardManagement from './pages/FarmManagement'
+import ModelsReport from './pages/ModelsReport'
+import ConnectDrone from './components/connectDrone'
+import ProtectedRoute from './components/ProtectedRoute'
+import OrchardPage from './pages/OrchardPage'
+import Chatbot from './components/chatbot/Chatbot'
+import FarmCard from './components/FarmCard'
+import QuickActions from './pages/QuickActions'
+import Analysis from './components/Analysis'
+import Learn from './components/Learn'
+import { createUser, getUser } from './api'
+import { GoogleTranslate } from './components/Translate'
 
 function App() {
-  const [count, setCount] = useState(0)
+  return <AppContent />
+}
+function AppContent() {
+  const location = useLocation()
+  const isOrchardRoute = location.pathname === '/orchard'
+  const [activeTab, setActiveTab] = useState(null)
+  const [userData, setUserData] = useState(null)
+  const {
+    isAuthenticated,
+    isLoading,
+    loginWithRedirect,
+    getAccessTokenSilently,
+    user
+  } = useAuth0()
+  const navigate = useNavigate()
 
+  useEffect(() => {
+    const checkAuth = async () => {
+      try {
+        await getAccessTokenSilently() // Tries to silently authenticate
+      } catch (error) {
+        console.error('Silent authentication failed:', error)
+      }
+    }
+    checkAuth()
+  }, [getAccessTokenSilently])
+
+  useEffect(() => {
+    const updateUser = async () => {
+      try {
+        if (!isLoading && isAuthenticated) {
+          await userInit(user.email)
+          navigate('/farm-management/dashboard') // Redirect logged-in users
+        }
+      } catch (error) {
+        console.error('User init failed', error)
+      }
+    }
+    updateUser()
+  }, [isAuthenticated, isLoading])
+
+  const userInit = async (email) => {
+    try {
+      const userData = await getUser(email)
+      if (userData) setUserData(userData)
+      else {
+        const newUser = await createUser(user.email, user.name)
+        setUserData(newUser)
+      }
+    } catch (error) {
+      console.error('Failed to get access token:', error)
+    }
+  }
   return (
     <>
-    <BrowserRouter>
-      <HomePage/>
-    </BrowserRouter>
-      
-      
+      <Navbar
+        background={isOrchardRoute ? '#f4f4f4' : 'transparent'}
+        isAuthenticated={isAuthenticated}
+      />
+      <div className='sm:mt-20'>
+        <GoogleTranslate />
+        <Routes>
+          <Route path='/' element={<LandingPage />} />
+          <Route path='/ownerPage' element={<ProfilePage />} />
+
+          <Route
+            path='farm-management'
+            element={
+              <OrchardManagement
+                activeTab={activeTab}
+                setActiveTab={setActiveTab}
+              />
+            }
+          >
+            <Route path='analysis' element={<Analysis />} />
+            <Route path='dashboard' element={<FarmCard />} />
+            <Route path='learn' element={<Learn />} />
+            <Route
+              path='quick-actions'
+              element={
+                <QuickActions
+                  setActiveTab={setActiveTab}
+                  activeTab={activeTab}
+                />
+              }
+            />
+            <Route path='drone' element={<ConnectDrone />} />
+          </Route>
+          <Route path='/chatbot' element={<Chatbot />} />
+          <Route path='/models-report' element={<ModelsReport />} />
+          <Route path='/connect' element={<ConnectDrone />} />
+          <Route path='/orchard' element={<OrchardPage />} />
+        </Routes>
+      </div>
     </>
   )
 }
-
 export default App
