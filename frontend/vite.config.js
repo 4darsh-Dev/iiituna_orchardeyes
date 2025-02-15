1caa035567dab5a9df56176cd8262b28e6719c71
AmanSingh494 <amanforwork1@gmail.com>
2025-02-15 10:11:26 +0530

    Deleted pwa code

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,32 +4,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'Orchard Eyes',
-        short_name: 'orchy',
-        start_url: '/',
-        display: 'standalone',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        icons: [
-          {
-            src: '/icons/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icons/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [react()],
   assetsInclude: ['**/*.glb', '**/*.gltf'],
   server: {
     host: '0.0.0.0' // Allows connections from other devices
